4496d8119376c0b325fd79fab1ff07ba3a498a65
Uchechukwu Udemezue <udemezue008@gmail.com>
2024-02-18 16:46:13 +0100

    Added and modified some changes

--- content/urls.py
+++ content/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import PasswordResetView
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
@@ -13,7 +14,8 @@ urlpatterns = [
     path('like/<int:post_id>/', views.like_post, name='like_post'),
     path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
     path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
-    path('password-reset/', views.password_reset, name='password_reset'),
+    # path('password-reset/', views.password_reset, name='password_reset'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password-reset/confirm/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
     path('password-reset/complete/', views.password_reset_complete, name='password_reset_complete'),
     path('password-reset/done/', views.password_reset_done, name='password_reset_done'),
--- content/views.py
+++ content/views.py
@@ -1,5 +1,6 @@
 # from allauth.account.forms import AllAuthPasswordResetForm
 from allauth.account.views import PasswordSetView, PasswordChangeView
+from django.contrib.auth.views import PasswordResetView
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
@@ -14,7 +15,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.forms import PasswordResetForm
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth import get_user_model
@@ -177,8 +178,47 @@ def delete_comment(request, comment_id):
 #     return render(request, 'content/password_reset.html', {'form': form})
 
 
-def password_reset(request):
-    return PasswordSetView.as_view(template_name='content/custom_password_reset.html')(request)
+# def password_reset(request):
+#     return PasswordSetView.as_view(template_name='content/custom_password_reset.html')(request)
+# def password_reset(request):
+#     if request.method == 'POST':
+#         form = PasswordResetForm(request.POST)
+#         if form.is_valid():
+#             # Generate a password reset token
+#             user = form.get_user()
+#             uid = urlsafe_base64_encode(force_bytes(user.pk))
+#             token = default_token_generator.make_token(user)
+
+#             # Construct the password reset link
+#             reset_url = request.build_absolute_uri(
+#                 f'/password-reset/confirm/{uid}/{token}/'
+#             )
+
+#             # Send a password reset email (customize this based on your email sending setup)
+#             send_mail(
+#                 'Password Reset',
+#                 f'Click the following link to reset your password: {reset_url}',
+#                 'from@example.com',
+#                 [user.email],
+#                 fail_silently=False,
+#             )
+
+#             messages.success(request, 'Password reset email sent. Check your inbox for further instructions.')
+#             return redirect('content:home')  # Customize the redirect URL after sending the email
+#     else:
+#         form = PasswordResetForm()
+#     return render(request, 'content/password_reset.html', {'form': form})
+    
+
+class PasswordResetView(PasswordResetView):
+    template_name = 'content/password_reset.html'
+    email_template_name = 'content/password_reset_email.html'
+    success_url = reverse_lazy('content:password_reset_done')
+
+    def get_success_url(self):
+        
+        return reverse_lazy('content:home')
+    
 
 
 def password_reset_confirm(request, uidb64, token):
