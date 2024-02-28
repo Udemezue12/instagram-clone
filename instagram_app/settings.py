f7607982a9ce42cfae82c9a12f32b8eb9b463ec7
Uchechukwu Udemezue <udemezue008@gmail.com>
2024-02-29 00:37:50 +0100

    modified some things

--- instagram_app/settings.py
+++ instagram_app/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.cigmangroup.com', 'cigmangroup.com', '*']
+CRSF_TRUSTED_ORIGINS= ['https://www.cigmangroup.com', 'https://cigmangroup.com']
 
 
 # Application definition
